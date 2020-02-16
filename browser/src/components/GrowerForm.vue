3fdaf1e9b8af41a862012624331c36a3923862ce
Regan <tarar@kiva.org>
2020-02-15 17:25:25 -0700

    Added form inputs

--- browser/src/components/GrowerForm.vue
+++ browser/src/components/GrowerForm.vue
@@ -3,9 +3,72 @@
         <h2>
           🐶🐶🐶 Hello Grower 🐶🐶🐶
         </h2>
+        <h3>
+            Please enter the information for your crop sample
+        </h3>
         <form id="submission" @submit.prevent="handleSubmit">
-        <!-- Please enter the information for your crop -->
         <p>
+            Size
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Date
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Laboratory
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Method
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Range
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Result (D9-THC)
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Valid Until
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <h3>
+            Harvest information
+        </h3>
+        <p>
+            Size (actual)
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        <p>
+            Pass
+            <select v-model="pass" >
+            <option value='Yes'>Yes </option>
+            <option value='No'>No</option>
+            </select>
+            {{pass}}
+        </p>
+        <p>
+            Destroyed
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Sold
+            <input type="text" v-model="msg" />
+            {{this.msg}}
+        </p>
+        <p>
+            Shipped
             <input type="text" v-model="msg" />
             {{this.msg}}
         </p>
@@ -22,9 +85,10 @@
         name: 'GrowerForm',
         data() {
             return {
-                msg: ''
-            }
-        },
+                msg: '',
+                pass: 'No'
+                }
+            },
         methods: {
             handleSubmit() {
                 console.log(this.msg)
